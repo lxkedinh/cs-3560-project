bc9a6e925168446b7ef5be9f8520e311b7dcb057
Luke Dinh <lukexdinh@gmail.com>
2023-05-14 13:57:31 -0700

    implemented fetching all loans and overdue loans

--- app/src/main/java/cs_3560_project/server/controllers/LoanController.java
+++ app/src/main/java/cs_3560_project/server/controllers/LoanController.java
@@ -1,6 +1,7 @@
 package cs_3560_project.server.controllers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.SessionFactory;
 import cs_3560_project.server.dao.DAO;
@@ -8,7 +9,9 @@ import cs_3560_project.server.dao.EntityNotFoundException;
 import cs_3560_project.server.model.Item;
 import cs_3560_project.server.model.ItemStatus;
 import cs_3560_project.server.model.Loan;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class LoanController {
 
@@ -29,36 +32,44 @@ public class LoanController {
     DAO.delete(loanToDelete);
   }
 
-  public static List<Loan> fetchOverdueLoans() {
-    var loansWrapper = new Object() {
-      List<Loan> overdueLoans = null;
+  public static List<Loan> fetchOverdueLoans() throws EntityNotFoundException {
+    List<Loan> allLoans = fetchAllLoans();
+    List<Loan> overdueLoans = new ArrayList<>();
+
+    for (Loan l : allLoans) {
+      if (l.getDueDate().compareTo(LocalDate.now()) < 0) {
+        overdueLoans.add(l);
+      }
+    }
+
+    if (overdueLoans.size() == 0) {
+      throw new EntityNotFoundException("There are no overdue loans.");
+    }
+
+    return overdueLoans;
+  }
+
+  public static List<Loan> fetchAllLoans() throws EntityNotFoundException {
+    var wrapper = new Object() {
+      List<Loan> loans = null;
     };
 
     SessionFactory factory = DAO.getFactory();
     factory.inTransaction(session -> {
-      session.enableFilter("overdueFilter");
-      CriteriaQuery<Loan> criteria = session.getCriteriaBuilder().createQuery(Loan.class);
-      loansWrapper.overdueLoans = session.createQuery(criteria).getResultList();
+      CriteriaBuilder cb = session.getCriteriaBuilder();
+      CriteriaQuery<Loan> cr = cb.createQuery(Loan.class);
+      Root<Loan> root = cr.from(Loan.class);
+      cr.select(root);
+      wrapper.loans = session.createQuery(cr).getResultList();
     });
 
-    return loansWrapper.overdueLoans;
+    if (wrapper.loans.size() == 0) {
+      throw new EntityNotFoundException("There are currently no loans in the system.");
+    }
+
+    return wrapper.loans;
   }
 
-  public static List<Loan> fetchAllLoans() {
-    var loansWrapper = new Object() {
-      List<Loan> overdueLoans = null;
-    };
-
-    SessionFactory factory = DAO.getFactory();
-    factory.inTransaction(session -> {
-      CriteriaQuery<Loan> criteria = session.getCriteriaBuilder().createQuery(Loan.class);
-      loansWrapper.overdueLoans = session.createQuery(criteria).getResultList();
-    });
-
-    return loansWrapper.overdueLoans;
-  }
-
-  // TODO: handle returning item
   public static void returnItem(int loanNumber) throws EntityNotFoundException {
     Loan loanToReturnItem = DAO.read(Loan.class, loanNumber);
     Item itemToReturn = loanToReturnItem.getItem();
